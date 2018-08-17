9c02f99eeec4a7cd4b48959079c4098fb9fee9d7
Lawrence Neal <nealla@lwneal.com>
2018-08-17 12:14:43 -0700

    Working (very simple toy) graph recovery

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -12,19 +12,17 @@ from logutil import TimeSeries
 class FourBitsEnv():
     # The environment consists of four bits
     def __init__(self):
-        self.state = np.random.randint(2, size=4)
+        #self.state = np.random.randint(2, size=4)
+        self.state = np.random.uniform(-1, 1, size=4)
 
     # The agent takes a binary action (0 or 1) which changes the state
     def step(self, a):
         assert 0 <= a <= 1
 
         # Bits 0, 1, 2, 3: W X Y Z
-        tmp = self.state[0]
-        self.state[0] = self.state[1]
-        self.state[1] = self.state[2]
-        self.state[2] = self.state[3]
-        self.state[3] = a
-
+        self.state[0], self.state[1], self.state[2], self.state[3] = \
+            a, self.state[0], self.state[1], self.state[2]
+        #self.state = np.clip(self.state, -1, 1)
 
 
 def build_dataset(size=10000):
@@ -39,7 +37,7 @@ def build_dataset(size=10000):
     return dataset  # list of tuples
 
 
-def get_batch(dataset, size=128):
+def get_batch(dataset, size=32):
     idx = np.random.randint(len(dataset) - size)
     inputs, actions, targets = zip(*dataset[idx:idx + size])
     state_act = np.concatenate([np.array(inputs), np.array(actions).reshape(-1, 1)], axis=1)
@@ -60,7 +58,7 @@ class Net(nn.Module):
         # Input: State + Action
         # Output: State
         self.fc1 = nn.Linear(4 + 1, 16, bias=False)
-        self.fc2 = nn.Linear(16, 4)
+        self.fc2 = nn.Linear(16, 4, bias=False)
         # one-layer version
         #self.fc1 = nn.Linear(5, 4)
 
@@ -68,7 +66,7 @@ class Net(nn.Module):
         x = self.fc1(x)
         x = F.leaky_relu(x, 0.2)
         x = self.fc2(x)
-        x = F.sigmoid(x * 10)
+        x = F.tanh(x)
         return x
 
 # This function identifies the non-pruned connections
@@ -77,10 +75,10 @@ def compute_causal_graph(model):
     rows = []
     for i in range(5):
         x = torch.zeros(5)
-        zero_return = model(x) #torch.matmul(model.fc2.weight.abs(), torch.matmul(model.fc1.weight.abs(), x))
+        zero_return = torch.matmul(model.fc2.weight.abs(), torch.matmul(model.fc1.weight.abs(), x))# + model.fc2.bias
         x[i] = 1.
         # Continuous version
-        one_return = model(x) # torch.matmul(model.fc2.weight.abs(), torch.matmul(model.fc1.weight.abs(), x))
+        one_return = torch.matmul(model.fc2.weight.abs(), torch.matmul(model.fc1.weight.abs(), x))# + model.fc2.bias
         # One-layer version
         #result = torch.matmul(model.fc1.weight.abs(), x)
         # Thresholded version
@@ -92,6 +90,7 @@ def compute_causal_graph(model):
     #scm -= scm.min()
     eps = .0001
     return scm / (scm.max() + eps)
+    #return scm
 
 
 # Simpler than the causal graph, least squares regression
@@ -155,7 +154,7 @@ def render_causal_graph(scm):
 # ok now, can the network learn the task?
 data = build_dataset()
 model = Net()
-optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.90)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 iters = 1000 * 1000
 ts = TimeSeries('Training', iters)
 
@@ -163,13 +162,14 @@ for i in range(iters):
     model.zero_grad()
     input, target = get_batch(data)
     predicted = model(input)
-    pred_loss = F.binary_cross_entropy(predicted, target)
-    #pred_loss = torch.sum((predicted - target) ** 2)
+    #pred_loss = F.binary_cross_entropy(predicted, target)
+    pred_loss = torch.sum((predicted - target) ** 2)
     ts.collect('Prediction loss', pred_loss)
 
     l1_loss = 0.
-    for param in model.parameters():
-        l1_loss += .1 * F.l1_loss(param, torch.zeros(param.shape))
+    l1_loss += 2.0 * F.l1_loss(model.fc1.weight, torch.zeros(model.fc1.weight.shape))
+    l1_loss += 2.0 * F.l1_loss(model.fc2.weight, torch.zeros(model.fc2.weight.shape))
+
     ts.collect('Sparsity loss', l1_loss)
 
     loss = pred_loss + l1_loss
@@ -179,6 +179,9 @@ for i in range(iters):
         print(compute_causal_graph(model))
         scm = compute_causal_graph(model)
         imutil.show(render_causal_graph(scm))
+        print(model.fc1.weight)
+        print(model.fc2.weight)
+        print(model.fc2.bias)
     ts.print_every(1)
     optimizer.step()
 
