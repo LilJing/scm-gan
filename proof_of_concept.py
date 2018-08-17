093d1ef71a2fa84d13479cb7728df6e5b125ef57
Lawrence Neal <nealla@lwneal.com>
2018-08-17 02:30:52 -0700

    Attempt at graph recovery

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,3 +1,4 @@
+
 import os
 import json
 import numpy as np
@@ -17,29 +18,19 @@ class FourBitsEnv():
     def step(self, a):
         assert 0 <= a <= 1
 
-        # Bit 0 is random and unaffected by anything else
-        self.state[0] = np.random.randint(2)
-
-        # Bit 1 follows bit 3, one time step behind
-        self.state[1] = self.state[3]
-
-        # Bit 2 is correlated to itself and the action
-        self.state[2] = self.state[2] if np.random.randint(2) else a
-
-        # Bit 3 is determined entirely by the agent's action
+        # Bits 0, 1, 2, 3: W X Y Z
+        tmp = self.state[0]
+        self.state[0] = self.state[1]
+        self.state[1] = self.state[2]
+        self.state[2] = self.state[3]
         self.state[3] = a
 
-        # A clever agent will learn the following structural causal model:
-        # 3 causes 1
-        # 0 causes 2
-        # A causes 2
-        # A causes 3
 
 
 def build_dataset(size=10000):
     dataset = []
-    env = FourBitsEnv()
     for i in tqdm(range(size)):
+        env = FourBitsEnv()
         before = np.array(env.state)
         action = np.random.randint(2)
         env.step(action)
@@ -48,7 +39,7 @@ def build_dataset(size=10000):
     return dataset  # list of tuples
 
 
-def get_batch(dataset, size=32):
+def get_batch(dataset, size=128):
     idx = np.random.randint(len(dataset) - size)
     inputs, actions, targets = zip(*dataset[idx:idx + size])
     state_act = np.concatenate([np.array(inputs), np.array(actions).reshape(-1, 1)], axis=1)
@@ -68,58 +59,133 @@ class Net(nn.Module):
         super().__init__()
         # Input: State + Action
         # Output: State
-        self.fc1 = nn.Linear(4 + 1, 32)
-        self.fc2 = nn.Linear(32, 4)
+        self.fc1 = nn.Linear(4 + 1, 128)
+        self.fc2 = nn.Linear(128, 4)
+        # one-layer version
+        #self.fc1 = nn.Linear(5, 4)
 
     def forward(self, x):
         x = self.fc1(x)
-        x = F.leaky_relu(x, 0.2)
+        #x = F.leaky_relu(x, 0.2)
+        x = F.relu(x)
         x = self.fc2(x)
-        x = F.sigmoid(x)
+        x = F.sigmoid(x * 10)
         return x
+
+# This function identifies the non-pruned connections
+def compute_causal_graph(model):
+    input_size = 5
+    rows = []
+    for i in range(5):
+        x = torch.zeros(5)
+        zero_return = torch.matmul(model.fc2.weight.abs(), torch.matmul(model.fc1.weight.abs(), x))
+        x[i] = 1.
+        # Continuous version
+        one_return = torch.matmul(model.fc2.weight.abs(), torch.matmul(model.fc1.weight.abs(), x))
+        # One-layer version
+        #result = torch.matmul(model.fc1.weight.abs(), x)
+        # Thresholded version
+        def binarize(W, theta=.01):
+            return (W.abs() > theta).type(torch.FloatTensor)
+        #result = torch.matmul(binarize(model.fc2.weight), torch.matmul(binarize(model.fc1.weight), x))
+        rows.append(np.array((one_return - zero_return).cpu().data))
+    scm = np.array(rows)
+    scm -= scm.min()
+    eps = .0001
+    return scm / (scm.max() + eps)
+
+
+# Simpler than the causal graph, least squares regression
+import pandas as pd
+def ordinary_least_squares():
+    inputs, targets = get_batch(data, size=5000)
+    predictions = model(inputs)
+    df = pd.concat([pd.DataFrame(np.array(inputs)), pd.DataFrame(np.array(targets))], axis=1)
+    df.columns = ['w', 'x', 'y', 'z', 'a', 'wn', 'xn', 'yn', 'zn']
+
+    from statsmodels.api import OLS
+    ols = OLS(df['a'], df[['wn', 'xn', 'yn', 'zn']]).fit()
+    print("Which dimensions of the state space correlate with our actions?")
+    print(ols.summary())
+
+
+# Just a helper function to render the graph to an image
+def render_causal_graph(scm):
+    # Headless matplotlib fix
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+
+    import networkx as nx
+
+    plt.cla()
+    #G = nx.generators.directed.random_k_out_graph(10, 3, 0.5)
+    # Make the scm into a square adjacency matrix
+    adjacency = np.zeros((5,5))
+    adjacency[:4,:] = scm.transpose()
+    adjacency = adjacency.transpose()
+    print(adjacency)
+    edge_alphas = adjacency.flatten()
+
+    from networkx.classes.multidigraph import DiGraph
+    G = DiGraph(np.ones((5,5)))
+
+    pos = nx.layout.circular_layout(G)
+
+    node_sizes = [10 for i in range(len(G))]
+    M = G.number_of_edges()
+    #edge_colors = range(2, M + 2)
+    #edge_alphas = [(5 + i) / (M + 4) for i in range(M)]
+    #edge_colors = [2 for i in range(len(G))]
+
+    nodes = nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='blue')
+    edges = nx.draw_networkx_edges(G, pos, node_size=node_sizes, arrowstyle='->', arrowsize=20, edge_cmap=plt.cm.Blues, width=2)
+    labels = ['$z_0$', '$z_1$', '$z_2$', '$z_3$', 'a']
+    labels = {i: labels[i] for i in range(len(labels))}
+    pos = {k: (v[0], v[1] + .1) for (k,v) in pos.items()}
+    nx.draw_networkx_labels(G, pos, labels, font_size=16)
+    # set alpha value for each edge
+    for i in range(M):
+        edges[i].set_alpha(edge_alphas[i])
+        ax = plt.gca()
+        ax.set_axis_off()
+        plt.show()
+    return imutil.show(plt, return_pixels=True, display=False)
 
 
 # ok now, can the network learn the task?
 data = build_dataset()
 model = Net()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-iters = 20 * 1000
+optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.90)
+iters = 1000 * 1000
 ts = TimeSeries('Training', iters)
 
 for i in range(iters):
     model.zero_grad()
     input, target = get_batch(data)
     predicted = model(input)
-    bce = F.binary_cross_entropy(predicted, target)
-    ts.collect('BCE loss', bce)
-    #loss = torch.sum((predicted - target) ** 2)
+    pred_loss = F.binary_cross_entropy(predicted, target)
+    #pred_loss = torch.sum((predicted - target) ** 2)
+    ts.collect('Prediction loss', pred_loss)
 
     l1_loss = 0.
     for param in model.parameters():
-        l1_loss += .1 * F.l1_loss(param, torch.zeros(param.shape))
-    ts.collect('L1 reg loss', l1_loss)
+        l1_loss += .01 * F.l1_loss(param, torch.zeros(param.shape))
+    ts.collect('Sparsity loss', l1_loss)
 
-    loss = bce + l1_loss
+    loss = pred_loss + l1_loss
 
     loss.backward()
     if i % 10000 == 0:
-        print(input[:4])
-        print(target[:4])
-        print(predicted[:4])
+        print(compute_causal_graph(model))
+        scm = compute_causal_graph(model)
+        imutil.show(render_causal_graph(scm))
     ts.print_every(1)
     optimizer.step()
 
 print(ts)
 
-import pandas as pd
-
-inputs, targets = get_batch(data, size=5000)
-predictions = model(inputs)
-df = pd.concat([pd.DataFrame(np.array(inputs)), pd.DataFrame(np.array(targets))], axis=1)
-df.columns = ['in0', 'in1', 'in2', 'in3', 'a', 'out0', 'out1', 'out2', 'out3']
-
-from statsmodels.api import OLS
-ols = OLS(df['in0'], df[['out0', 'out1', 'out2', 'out3']]).fit()
-
 import pdb; pdb.set_trace()
 
+scm = compute_causal_graph(model)
+imutil.show(render_causal_graph(scm))
