69936cc4bc542d4d0e3936aacb7117b291c29784
Lawrence Neal <nealla@lwneal.com>
2018-09-30 17:29:40 -0700

    Update SRN, fix convolutions

--- spatial_recurrent.py
+++ spatial_recurrent.py
@@ -9,58 +9,59 @@ class CSRN(nn.Module):
     def __init__(self, height, width, channels):
         super().__init__()
         self.channels = channels
-        self.gru_in = 3
-        self.gru_out = 3
-        self.conv_rows = nn.Conv1d(channels, self.gru_in, kernel_size=3, stride=1, padding=1)
-        self.conv_cols = nn.Conv1d(channels, self.gru_in, kernel_size=3, stride=1, padding=1)
-        self.gru_rows = nn.GRU(self.gru_in, self.gru_out)
-        self.gru_cols = nn.GRU(self.gru_in, self.gru_out)
-        self.conv_combine = nn.Conv2d(self.gru_out * 2, channels, kernel_size=1)
+        self.rnn_in = channels
+        self.rnn_out = 3
+        self.conv_rows = nn.Conv1d(self.rnn_out, self.rnn_in, kernel_size=3, stride=1, padding=1)
+        self.conv_cols = nn.Conv1d(self.rnn_out, self.rnn_in, kernel_size=3, stride=1, padding=1)
+        self.rnn_rows = nn.GRU(self.rnn_in, self.rnn_out)
+        self.rnn_cols = nn.GRU(self.rnn_in, self.rnn_out)
+        self.conv_combine = nn.Conv2d(self.rnn_out * 2, channels, kernel_size=1)
         #self.conv_rows.weight.data += 1
+        #self.conv_rows.weight.data[-1][:2] = 0
+        #self.conv_cols.weight.data[-1][:2] = 0
 
     def forward(self, x):
         batch_size, channels, height, width = x.shape
         assert channels == self.channels
         # x.shape: (batch, channels, height, width)
-        row_wise = torch.zeros((batch_size, height, width, self.gru_out))
-        width_h0 = torch.zeros((1, batch_size * width, self.gru_out))
-        col_wise = torch.zeros((batch_size, height, width, self.gru_out))
-        height_h0 = torch.zeros((1, batch_size * height, self.gru_out))
+        context_above = torch.zeros((batch_size, self.rnn_out, height, width))
+        context_left = torch.zeros((batch_size, self.rnn_out, height, width))
 
-        # Run vertically
+        # For each row, top to bottom:
+        #  Take the output of the RNN on the previous row
+        #  Apply a 1D convolution to that row (so activations spread like a cone)
+        #  Feed the convolved activations back into the RNN
+        rnn_state = torch.zeros((1, batch_size * width, self.rnn_in))
         for i in range(height):
-            rows_in = x[:, :, i]
-            # (batch, channels, width)
-            conv_row = self.conv_rows(rows_in)
-            conv_row = torch.tanh(conv_row)
-            # (batch, self.gru_in, width)
-            gru_input = conv_row.permute(0,2,1).contiguous().view(-1, self.gru_in)
-            # (batch * width, self.gru_in)
-            gru_input = gru_input.unsqueeze(0)
-            # (1, batch * width, self.gru_in)
-            rnn_out, width_h0 = self.gru_rows(gru_input, width_h0)
-            row_wise[:, i] = rnn_out.view(batch_size, width, self.gru_out)
-        imutil.show(row_wise[0])
+            pixel_row = x[:, :, i, :]
+            pixel_row = pixel_row.permute(0, 2, 1).contiguous()
+            pixel_row = pixel_row.view(1, batch_size * width, self.rnn_in)
+            rnn_out, rnn_state = self.rnn_rows(pixel_row, rnn_state)
+            conv_in = rnn_out.view(batch_size, width, self.rnn_in)
+            conv_in = conv_in.permute(0, 2, 1)
+            context_above[:, :, i, :] = conv_in
+            conv_out = self.conv_rows(conv_in)
+            conv_out = torch.tanh(conv_out)
+            rnn_state = conv_out.permute(0, 2, 1).contiguous()
+            rnn_state = rnn_state.view(1, batch_size * width, self.rnn_in)
 
-        # Run horizontally
+        rnn_state = torch.zeros((1, batch_size * height, self.rnn_in))
         for i in range(width):
-            cols_in = x[:, :, :, i]
-            # (batch, channels, height)
-            conv_col = self.conv_cols(cols_in)
-            conv_col = torch.tanh(conv_col)
+            pixel_col = x[:, :, :, i]
+            pixel_col = pixel_col.permute(0, 2, 1).contiguous()
+            pixel_col = pixel_col.view(1, batch_size * height, self.rnn_in)
+            rnn_out, rnn_state = self.rnn_cols(pixel_col, rnn_state)
+            conv_in = rnn_out.view(batch_size, height, self.rnn_in)
+            conv_in = conv_in.permute(0, 2, 1)
+            context_left[:, :, :, i] = conv_in
+            conv_out = self.conv_cols(conv_in)
+            conv_out = torch.tanh(conv_out)
+            rnn_state = conv_out.permute(0, 2, 1).contiguous()
+            rnn_state = rnn_state.view(1, batch_size * height, self.rnn_in)
+        import pdb; pdb.set_trace()
 
-            # (batch, self.gru_in, height)
-            gru_input = conv_col.permute(0,2,1).contiguous().view(-1, self.gru_in)
-            # (batch * height, self.gru_in)
-            gru_input = gru_input.unsqueeze(0)
-            # (1, batch * height, self.gru_in)
-            rnn_out, height_h0 = self.gru_cols(gru_input, height_h0)
-            col_wise[:, :, i] = rnn_out.view(batch_size, height, self.gru_out)
-        imutil.show(col_wise[0])
-
-        gru_output = torch.cat((row_wise, col_wise), dim=-1)
-        gru_output = gru_output.permute((0, 3, 1, 2))
-        return torch.sigmoid(self.conv_combine(gru_output))
+        context_map = torch.cat((context_above, context_left), dim=1)
+        return torch.sigmoid(self.conv_combine(context_map))
 
 
 img = imutil.show('kittenpuppy.jpg', return_pixels=True)
