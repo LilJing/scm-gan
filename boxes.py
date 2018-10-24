de9ad535f68c932852364367475ed968a6af2a08
Lawrence Neal <nealla@lwneal.com>
2018-10-24 08:18:12 -0700

    Add moving boxes environment

---
+++ boxes.py
@@ -0,0 +1,83 @@
+import time
+import numpy as np
+import random
+
+from tqdm import tqdm
+
+GAME_SIZE = 64
+dataset = None
+
+def init():
+    global dataset
+    dataset = build_dataset()
+
+
+class BoxesEnv():
+    def __init__(self):
+        self.width = np.random.uniform(5, 20)
+        self.height = np.random.randint(5, 20)
+        self.x = np.random.randint(20, 46)
+        self.y = np.random.randint(20, 46)
+        self.state = build_state(self.width, self.height, self.x, self.y)
+
+    # The agent can press one of four buttons
+    def step(self, a):
+        # Some dimensions change in reaction to the agent's actions
+        if a[0]:
+            self.x -= 3
+        elif a[1]:
+            self.x += 3
+
+        if a[2]:
+            self.y -= 3
+        elif a[3]:
+            self.y += 3
+        # Other dimensions change, but not based on agent actions
+        self.width -= 3
+        self.state = build_state(self.width, self.height, self.x, self.y)
+
+
+# Continuous inputs are in the range [0, 1] for each dimension
+def generate_image_continuous(factors):
+    width = int(factors[0] * 15) + 5
+    height = int(factors[1] * 15) + 5
+    x = int(factors[2] * 26) + 20
+    y = int(factors[3] * 26) + 20
+    return build_state(width, height, x, y)
+
+
+def build_state(width, height, x, y):
+    state = np.zeros((GAME_SIZE, GAME_SIZE))
+    y0 = int(y - height)
+    y1 = int(y + height)
+    x0 = int(x - width)
+    x1 = int(x + width)
+    state[y0:y1, x0:x1] = 1.0
+    return state
+
+
+def build_dataset(num_actions=4, size=50000):
+    dataset = []
+    for i in tqdm(range(size)):
+        env = BoxesEnv()
+        before = np.array(env.state)
+        action = np.zeros(shape=(num_actions,))
+        action[np.random.randint(num_actions)] = 1.
+        env.step(action)
+        after = np.array(env.state)
+        dataset.append((before, action, after))
+    return dataset  # list of tuples
+
+
+def get_batch(size=32):
+    idx = np.random.randint(len(dataset) - size)
+    inputs, actions, targets = zip(*dataset[idx:idx + size])
+    return inputs, actions, targets
+
+
+def simulator(factor_batch):
+    batch_size = len(factor_batch)
+    images = []
+    for i in range(batch_size):
+        images.append(generate_image_continuous(factor_batch[i]))
+    return np.array(images)
---
+++ boxes_causal_autoencoder.py
@@ -0,0 +1,168 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import imutil
+from logutil import TimeSeries
+from tqdm import tqdm
+
+import boxes
+from higgins import higgins_metric
+
+
+class Transition(nn.Module):
+    def __init__(self, latent_size, num_actions):
+        super().__init__()
+        # Input: State + Action
+        # Output: State
+        self.fc1 = nn.Linear(latent_size + num_actions, 16, bias=False)
+        self.fc2 = nn.Linear(16, latent_size, bias=False)
+        self.cuda()
+
+    def forward(self, z, actions):
+        x = torch.cat([z, actions], dim=1)
+        x = self.fc1(x)
+        x = F.leaky_relu(x, 0.2)
+        x = self.fc2(x)
+        x = torch.tanh(x)
+        # Fern hack: Predict a delta/displacement
+        return z + x
+
+
+class Encoder(nn.Module):
+    def __init__(self, latent_size):
+        super().__init__()
+        self.latent_size = latent_size
+        # Bx1x64x64
+        self.fc1 = nn.Linear(64*64, 1200)
+        self.bn1 = nn.BatchNorm1d(1200)
+        self.fc2 = nn.Linear(1200, 1200)
+        self.bn2 = nn.BatchNorm1d(1200)
+        self.fc3 = nn.Linear(1200, latent_size * 2)
+
+        # Bxlatent_size
+        self.cuda()
+
+    def forward(self, x):
+        # Input: B x 1 x 64 x 64
+        x = x.view(-1, 64*64)
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+
+        x = self.fc3(x)
+        return x[:,:self.latent_size], x[:, self.latent_size:]
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_size):
+        super().__init__()
+        self.latent_size = latent_size
+        self.fc1 = nn.Linear(latent_size, 1200)
+        self.bn1 = nn.BatchNorm1d(1200)
+        self.fc2 = nn.Linear(1200, 1200)
+        self.bn2 = nn.BatchNorm1d(1200)
+        self.fc3 = nn.Linear(1200, 1200)
+        self.bn3 = nn.BatchNorm1d(1200)
+        self.fc4 = nn.Linear(1200, 4096)
+        # B x 1 x 64 x 64
+        self.cuda()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = F.leaky_relu(x, 0.2)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.leaky_relu(x, 0.2)
+        x = self.fc3(x)
+        x = self.bn3(x)
+        x = F.leaky_relu(x, 0.2)
+        x = self.fc4(x)
+        #x = torch.sigmoid(x)
+        x = x.view(-1, 1, 64, 64)
+        return x
+
+
+def main():
+    boxes.init()
+
+    # Compute Higgins metric for a randomly-initialized convolutional encoder
+    batch_size = 32
+    latent_dim = 4
+    true_latent_dim = 4
+    num_actions = 4
+    encoder = Encoder(latent_dim)
+    decoder = Decoder(latent_dim)
+    transition = Transition(latent_dim, num_actions)
+    higgins_scores = []
+
+    # Train the autoencoder
+    opt_enc = torch.optim.Adagrad(encoder.parameters(), lr=.01)
+    opt_dec = torch.optim.Adagrad(decoder.parameters(), lr=.01)
+    opt_trans = torch.optim.Adagrad(transition.parameters(), lr=.01)
+    train_iters = 200 * 1000
+    ts = TimeSeries('Training Autoencoder', train_iters)
+    for train_iter in range(train_iters + 1):
+        encoder.train()
+        decoder.train()
+
+        images, actions, images_tplusone = boxes.get_batch()
+        opt_enc.zero_grad()
+        opt_dec.zero_grad()
+        x = torch.Tensor(images).cuda().unsqueeze(1)
+        a_t = torch.Tensor(actions).cuda()
+        x_tplusone = torch.Tensor(images_tplusone).cuda().unsqueeze(1)
+        z, _ = encoder(x)
+        reconstructed_logits = decoder(z)
+        reconstructed = torch.sigmoid(reconstructed_logits)
+
+        #recon_loss = torch.sum((x - reconstructed) ** 2)
+        recon_loss = F.binary_cross_entropy_with_logits(reconstructed_logits, x, reduction='sum')
+        ts.collect('Reconstruction loss', recon_loss)
+
+        z_tplusone = transition(z, a_t)
+        predicted_logits = decoder(z_tplusone)
+        prediction_loss = F.binary_cross_entropy_with_logits(predicted_logits, x_tplusone, reduction='sum')
+        ts.collect('Prediction loss', prediction_loss)
+
+        l1_scale = 10.0 * (train_iter / train_iters)
+        l1_loss = 0.
+        l1_loss += l1_scale * F.l1_loss(transition.fc1.weight, torch.zeros(transition.fc1.weight.shape).cuda())
+        l1_loss += l1_scale * F.l1_loss(transition.fc2.weight, torch.zeros(transition.fc2.weight.shape).cuda())
+        ts.collect('Sparsity loss', l1_loss)
+
+        loss = recon_loss + prediction_loss + l1_loss
+        loss.backward()
+        opt_enc.step()
+        opt_dec.step()
+        opt_trans.step()
+        ts.print_every(2)
+
+        encoder.eval()
+        decoder.eval()
+        transition.eval()
+
+        if train_iter % 1000 == 0:
+            filename = 'vis_iter_{:06d}.jpg'.format(train_iter)
+            img = torch.cat((x[:4], reconstructed[:4]), dim=3)
+            imutil.show(img, filename=filename, caption='D(E(x)) iter {}'.format(train_iter), font_size=10)
+        if train_iter % 10000 == 0:
+            # Compute metric again after training
+            trained_score = higgins_metric(boxes.simulator, true_latent_dim, encoder, latent_dim)
+            higgins_scores.append(trained_score)
+            print('Higgins metric before training: {}'.format(higgins_scores[0]))
+            print('Higgins metric after training {} iters: {}'.format(train_iter, higgins_scores[-1]))
+            ts.collect('Higgins Metric', trained_score)
+    print(ts)
+    print('Finished')
+
+
+if __name__ == '__main__':
+    main()
