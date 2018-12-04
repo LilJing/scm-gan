3e354162b095b67b67d0949bfb0a267eb5a50a7e
Lawrence Neal <nealla@lwneal.com>
2018-12-04 12:24:41 -0800

    Update Minipong env to support online training
    For the Higgins metric we still want the on-demand batch mode, but
    standard training will now take place with an online simulator.

--- minipong.py
+++ minipong.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 
 from tqdm import tqdm
+from multi_env import MultiEnvironment
+from gym.spaces.discrete import Discrete
 
 GAME_SIZE = 64
 dataset = None
@@ -14,6 +16,10 @@ def init():
 
 class MinipongEnv():
     def __init__(self):
+        self.reset()
+        self.action_space = Discrete(4)
+
+    def reset(self):
         self.left_y = np.random.randint(0, 64)
         self.right_y = np.random.randint(0, 64)
         self.ball_x = np.random.randint(0, 64)
@@ -23,22 +29,23 @@ class MinipongEnv():
     # The agent can press one of four buttons
     def step(self, a):
         # Some dimensions change in reaction to the agent's actions
-        if a[0]:
+        if a == 0:
             self.right_y -= 3
-        elif a[1]:
+        elif a == 1:
             self.right_y += 3
         self.right_y %= 64
 
-        if a[2]:
+        if a == 2:
             self.left_y -= 3
-        elif a[3]:
+        elif a == 3:
             self.left_y += 3
         self.left_y %= 64
 
         # Other dimensions change, but not based on agent actions
-        self.ball_x += 2
+        self.ball_x += 3
         self.ball_x %= 64
         self.state = build_state(self.left_y, self.right_y, self.ball_x, self.ball_y)
+        return self.state, 0, False, {}
 
 
 def build_state(left_y, right_y, ball_x, ball_y):
@@ -68,8 +75,7 @@ def build_dataset(num_actions=4, size=50000):
     for i in tqdm(range(size)):
         env = MinipongEnv()
         before = np.array(env.state)
-        action = np.zeros(shape=(num_actions,))
-        action[np.random.randint(num_actions)] = 1.
+        action = np.random.randint(num_actions)
         env.step(action)
         after = np.array(env.state)
         dataset.append((before, action, after))
@@ -84,10 +90,10 @@ def get_batch(size=32):
 
 # Continuous inputs are in the range [0, 1] for each dimension
 def generate_image_continuous(factors):
-    left_y = int(factors[0] * 40) + 10
-    right_y = int(factors[1] * 40) + 10
-    ball_x = int(factors[2] * 60) + 2
-    ball_y = int(factors[3] * 60) + 2
+    left_y = int(factors[0] * GAME_SIZE) + 0
+    right_y = int(factors[1] * GAME_SIZE) + 0
+    ball_x = int(factors[2] * GAME_SIZE) + 0
+    ball_y = int(factors[3] * GAME_SIZE) + 0
     return build_state(left_y, right_y, ball_x, ball_y)
 
 
@@ -97,3 +103,21 @@ def simulator(factor_batch):
     for i in range(batch_size):
         images.append(generate_image_continuous(factor_batch[i]))
     return np.array(images)
+
+
+def get_trajectories(batch_size=32, timesteps=10, policy=None):
+    envs = MultiEnvironment([MinipongEnv() for _ in range(batch_size)])
+    t_states, t_rewards, t_dones, t_actions = [], [], [], []
+    for t in range(timesteps):
+        actions = np.random.randint(envs.action_space.n, size=(batch_size,))
+        states, rewards, dones, _ = envs.step(actions)
+        t_states.append(states)
+        t_rewards.append(rewards)
+        t_dones.append(dones)
+        t_actions.append(actions)
+    # Reshape to (batch_size, timesteps, ...)
+    states = np.swapaxes(t_states, 0, 1)
+    rewards = np.swapaxes(t_rewards, 0, 1)
+    dones = np.swapaxes(t_dones, 0, 1)
+    actions = np.swapaxes(t_actions, 0, 1)
+    return states, rewards, dones, actions
