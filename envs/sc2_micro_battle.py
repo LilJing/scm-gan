2daa863d9a312f64ae92d11683c753877fe5aed3
Lawrence Neal <nealla@lwneal.com>
2019-02-14 14:38:14 -0800

    Fix sc2_micro_battle for 128x128
    It looks nice but it's too slow

--- envs/sc2_micro_battle.py
+++ envs/sc2_micro_battle.py
@@ -10,6 +10,7 @@ REPLAY_FACTOR = 8
 REPLAY_BUFFER_LEN = 500
 MAX_TRAJECTORY_LEN = 100
 MAX_EPISODES_PER_ENVIRONMENT = 500
+NUM_ACTIONS = 2
 replay_buffer = []
 env = None
 simulation_iters = 0
@@ -44,8 +45,7 @@ def play_episode(env, policy):
         state = state[3]  # Rendered game pixels
         state = state.transpose((2,0,1))  # HWC -> CHW
         state = state * (1/255)  # [0,1]
-        state[:, :80] = 0
-        state[:, -80:] = 0
+        state = state[:,::2,::2]
         states.append(state)
         rewards.append(reward)
         actions.append(action)
