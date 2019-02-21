663075ca0c0c02ee780036e2dbecd6827e044f29
Lawrence Neal <nealla@lwneal.com>
2019-02-21 11:21:33 -0800

    Update manual game-playing interface

--- server.py
+++ server.py
@@ -1,12 +1,34 @@
 import flask
 import imutil
 import gym
+import numpy as np
 
 app = flask.Flask(__name__)
 
-def reset_game(name='Breakout-v0'):
+class GameEnv(gym.Env):
+    def __init__(self, name):
+        super().__init__()
+        self.env = gym.make(name)
+        self.env.unwrapped.frameskip = 1
+        self.last_state = self.env.reset()
+
+    def step(self, action):
+        new_state, reward, done, info = self.env.step(action)
+
+        # Max the previous two frames to remove flickering
+        state = np.maximum(new_state, self.last_state)
+
+        self.last_state = new_state
+        return state, reward, done, info
+
+    def reset(self):
+        return self.env.reset()
+
+
+
+def reset_game():
     global env
-    env = gym.make(name)
+    env = GameEnv('CentipedeDeterministic-v4')
     state = env.reset()
     imutil.show(state, filename='static/screenshot.jpg')
 
@@ -20,7 +42,7 @@ def route_step():
     print('action from form is: {}'.format(action))
     state, reward, done, info = env.step(action)
     imutil.show(state, filename='static/screenshot.jpg')
-    frame_num = env.unwrapped.ale.getFrameNumber()
+    frame_num = env.env.unwrapped.ale.getFrameNumber()
     print('took action, now at frame num {}'.format(frame_num))
     if done:
         env.reset()
@@ -34,4 +56,4 @@ def route_static(path):
 
 if __name__ == '__main__':
     reset_game()
-    app.run('0.0.0.0', debug=True)
+    app.run('0.0.0.0', debug=True, threaded=False)
